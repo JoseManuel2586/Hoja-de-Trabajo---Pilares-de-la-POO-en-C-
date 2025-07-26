65a5eed409efe90500f8c6face845d6d53275b66
José Manuel López <jose_lopez2586@hotmail.com>
2025-07-26 16:16:33 -0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Hoja_de_Trabajo___Pilares_de_la_POO_en_C_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
