328a1614d47571376930f3c6ece59b85db45ca3c
José Manuel López <jose_lopez2586@hotmail.com>
2025-07-26 19:56:51 -0600

    Ejercicio 2: Usuario con validación de campos sensibles

---
+++ Ejercicio2/Usuario.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hoja_de_Trabajo___Pilares_de_la_POO_en_C_.Ejercicio2
+{
+    public class Usuario
+    {
+        private string dpi;
+
+        private string contrasenia;
+
+        public string Nombre { get; set; }
+
+        public string Dpi
+        {
+            get => dpi;
+            set
+            {
+                if (value.Length == 13 && long.TryParse(value, out _))
+                {
+                    dpi = value;
+                }
+                else
+                {
+                    throw new ArgumentException("El DPI debe contener 13 digitos numericos.");
+                }
+            }
+        }
+        public string Contrasenia
+        {
+            set
+            {
+                if (value.Length >= 6)
+                {
+                    contrasenia = value;
+                }
+                else
+                {
+                    throw new ArgumentException("La contraseña debe tener al menos 6 caracteres.");
+                }
+            }
+        }
+        public bool Autenticar(string input)
+        {
+            return input == contrasenia;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 ﻿using Hoja_de_Trabajo___Pilares_de_la_POO_en_C_.Clases;
+using Hoja_de_Trabajo___Pilares_de_la_POO_en_C_.Ejercicio2;
+using Hoja_de_Trabajo___Pilares_de_la_POO_en_C_.Ejercico3;
 
 namespace Hoja_de_Trabajo___Pilares_de_la_POO_en_C_
 {
@@ -17,7 +19,53 @@ namespace Hoja_de_Trabajo___Pilares_de_la_POO_en_C_
             biblioteca.MostrarLibros();
             biblioteca.PrestarLibro("crimen y castigo", "Jose Manuel Lopez");
 
+            Console.WriteLine("-------------------------------------------------------------------------------------------------");
+            try
+            {
+                Usuario user = new Usuario();
+                user.Nombre = "Jose Manuel";
+                user.Dpi = "1234567890123";
+                user.Contrasenia = "contrasenia123";
 
+                Console.WriteLine("Usuario creado exitosamente.");
+
+                Console.Write("Ingresa tu contraseña: ");
+                string entrada = Console.ReadLine();
+
+                if (user.Autenticar(entrada))
+                {
+                    Console.WriteLine("¡Autenticación exitosa!");
+                }
+                else
+                {
+                    Console.WriteLine("Contraseña incorrecta.");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error: {ex.Message}");
+            }
+            
+            Console.WriteLine("-------------------------------------------------------------------------------------------------");
+
+            Inventario inventarioProductos = new Inventario();
+
+            inventarioProductos.AgregarProducto("Pantalon", 50);
+            inventarioProductos.AgregarProducto("Patalon", 20);
+            inventarioProductos.AgregarProducto("Playera", 30);
+            inventarioProductos.MostrarInventario();
+
+            inventarioProductos.RetirarProducto("Pantalon", 40);
+            inventarioProductos.RetirarProducto("Playera", 30);
+            inventarioProductos.MostrarInventario();
+
+            inventarioProductos.RetirarProducto("Pantalon", 100);
+            inventarioProductos.AgregarProducto("Zapatos", -5);
+
+
+            Console.ReadLine();
         }
+
+
     }
 }
